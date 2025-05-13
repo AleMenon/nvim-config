c5a92c83063fe24c4313fc0e54061708bacff34d
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2025-05-12 23:33:22 -0300

    Identetion correction and changed neotree options to show hidden files

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,10 +1,10 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-    vim.keymap.set('n', '<leader>mk', ':RenderMarkdown toggle<CR>', { noremap = true, silent = true }) -- toggle markdown
+   'MeanderingProgrammer/render-markdown.nvim',
+   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+   ---@module 'render-markdown'
+   ---@type render.md.UserConfig
+   opts = {},
+   vim.keymap.set('n', '<leader>mk', ':RenderMarkdown toggle<CR>', { noremap = true, silent = true }) -- toggle markdown
 }
