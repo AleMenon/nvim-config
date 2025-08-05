5fe9d0554073a7e5bdeb247b5e5905fa06b71e1f
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2025-08-05 18:03:57 -0300

    none-ls removed, also changed the lsp for python

--- lua/plugins/lazy-setup.lua
+++ lua/plugins/lazy-setup.lua
@@ -25,7 +25,6 @@ return function()
       require 'plugins.render-markdown', -- Markdown render
       require 'plugins.pairs-and-surrounds', -- Autopair and surround plugins
       require 'plugins.oil', -- File creation and management
-      require 'plugins.none-ls', -- Autoformatting for python
       require 'plugins.which-key', -- Useful for finding keymaps you don't remember
       require 'plugins.flash', -- Teleportation in the code
       require 'plugins.todo-comments', -- Better comments
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -200,24 +200,8 @@ return {
          marksman = {},
          texlab = {},
          -- gopls = {},
-         -- pyright = {},
+         pyright = {},
          -- rust_analyzer = {},
-         pylsp = {
-            settings = {
-               pylsp = {
-                  plugins = {
-                     pyflakes = { enabled = false },
-                     pycodestyle = { enabled = false },
-                     autopep8 = { enabled = false },
-                     yapf = { enabled = false },
-                     mccabe = { enabled = false },
-                     pylsp_mypy = { enabled = false },
-                     pylsp_black = { enabled = false },
-                     pylsp_isort = { enabled = false },
-                  },
-               },
-            },
-         },
 
          -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
          --
