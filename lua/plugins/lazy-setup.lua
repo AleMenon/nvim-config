793dca99ac9303790714a143caf9682fa2d409d6
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2025-11-25 20:52:32 -0300

    New file manager added, substitute for oil.nvim

--- lua/plugins/lazy-setup.lua
+++ lua/plugins/lazy-setup.lua
@@ -1,36 +1,37 @@
 return function()
-   -- [[ Install `lazy.nvim` plugin manager ]]
-   local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-   if not (vim.uv or vim.loop).fs_stat(lazypath) then
-      local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-      local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-      if vim.v.shell_error ~= 0 then
-         error('Error cloning lazy.nvim:\n' .. out)
-      end
-   end
-   vim.opt.rtp:prepend(lazypath)
+    -- [[ Install `lazy.nvim` plugin manager ]]
+    local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+    if not (vim.uv or vim.loop).fs_stat(lazypath) then
+        local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+        local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+        if vim.v.shell_error ~= 0 then
+            error('Error cloning lazy.nvim:\n' .. out)
+        end
+    end
+    vim.opt.rtp:prepend(lazypath)
 
-   require('lazy').setup({
-      require 'plugins.colorschemes', -- Colorscheme plugins
-      require 'plugins.bufferline', -- Buffer manager
-      require 'plugins.lualine', -- Lualine
-      require 'plugins.treesitter', -- Highlighting
-      require 'plugins.telescope', -- fzf files explorer
-      require 'plugins.lsp', -- lsp configuration
-      require 'plugins.autocompletion', -- Text suggestions and autocompletion
-      require 'plugins.gitsigns', -- Git plugin
-      require 'plugins.alpha', -- Welcome mensage and recent files
-      require 'plugins.indent-blankline', -- Functions line
-      require 'plugins.render-markdown', -- Markdown render
-      require 'plugins.oil', -- File creation and management
-      require 'plugins.which-key', -- Useful for finding keymaps you don't remember
-      require 'plugins.flash', -- Teleportation in the code
-      require 'plugins.todo-comments', -- Better comments
-      require 'plugins.csv-view', -- CSV file render
-      require 'plugins.markdown-preview', -- Web markdown preview
-      require 'plugins.fugitive', -- Plugin to run git commands inside neovim
-      require 'plugins.gitgraph', -- Gitgraph
-      require 'plugins.toggleterm', -- Plugin for using scooter
-      require 'plugins.misc', -- Other useful plugins
-   })
+    require('lazy').setup({
+        require 'plugins.colorschemes', -- Colorscheme plugins
+        require 'plugins.bufferline', -- Buffer manager
+        require 'plugins.lualine', -- Lualine
+        require 'plugins.treesitter', -- Highlighting
+        require 'plugins.telescope', -- fzf files explorer
+        require 'plugins.lsp', -- lsp configuration
+        require 'plugins.autocompletion', -- Text suggestions and autocompletion
+        require 'plugins.gitsigns', -- Git plugin
+        require 'plugins.alpha', -- Welcome mensage and recent files
+        require 'plugins.indent-blankline', -- Functions line
+        require 'plugins.render-markdown', -- Markdown render
+        -- require 'plugins.oil', -- File creation and management
+        require 'plugins.which-key', -- Useful for finding keymaps you don't remember
+        require 'plugins.flash', -- Teleportation in the code
+        require 'plugins.todo-comments', -- Better comments
+        require 'plugins.csv-view', -- CSV file render
+        require 'plugins.markdown-preview', -- Web markdown preview
+        require 'plugins.fugitive', -- Plugin to run git commands inside neovim
+        require 'plugins.gitgraph', -- Gitgraph
+        require 'plugins.toggleterm', -- Plugin for using scooter
+        require 'plugins.misc', -- Other useful plugins
+        require 'plugins.fyler', -- File creation and management (Testing)
+    })
 end
