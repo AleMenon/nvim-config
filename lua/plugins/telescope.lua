80657edc9ba3707338e033732f3a1ace0db49f86
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2025-12-28 17:11:13 -0300

    Unused keymaps removed from telescope plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -115,24 +115,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>su', '<cmd>Telescope undo<cr>', { desc = '[S]earch [U]ndo'})
 
-      -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = false,
-         })
-      end, { desc = '[/] Fuzzily search in current buffer' })
-
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-         builtin.live_grep {
-            grep_open_files = true,
-            prompt_title = 'Live Grep in Open Files',
-         }
-      end, { desc = '[S]earch [/] in Open Files' })
-
       -- Shortcut for searching your Neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()
          builtin.find_files { cwd = vim.fn.stdpath 'config' }
