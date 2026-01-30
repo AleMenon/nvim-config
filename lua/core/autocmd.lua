a095c5f96ba36aa2c7c229941991963ea0b762be
Alexandre Alberto Menon <alexandre.menon@fkn.com.br>
2026-01-30 08:48:27 -0300

    New autocmd for session management

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -100,3 +100,15 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.keymap.set("i", "<Tab>", "    ", { buffer = true, noremap = true })
     end,
 })
+
+-- delete empty buffers when a session loads
+vim.api.nvim_create_autocmd("SessionLoadPost", {
+    callback = function ()
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.api.nvim_buf_is_loaded(buf)
+            and vim.api.nvim_buf_get_name(buf) == "" then
+                vim.api.nvim_buf_delete(buf, { force = true })
+            end
+        end
+    end,
+})
