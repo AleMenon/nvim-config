94aab3e67b8ab728431c6455930670ff9d037f9c
Alexandre Menon <alexandre.menon@fkn.com.br>
2026-07-29 11:29:32 -0300

    Adjustments for creating snippets

---
+++ lua/snippets/markdown.lua
@@ -0,0 +1,10 @@
+local ls = require 'luasnip'
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
+local fmt = require('luasnip.extras.fmt').fmt
+local rep = require('luasnip.extras').rep
+
+return {
+    -- Insert snippets here
+}
